21ec0955c3f92346ddb88345beadf9d4621b027d
Ranu Tanwar <ranutanwar1997@gmail.com>
2021-05-09 17:43:43 +0530

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+console.log("connected");
+            const menuToggle = document.querySelector('.toggle')
+            const showcase = document.querySelector('.showcase')
+
+            menuToggle.addEventListener('click', () =>{
+                menuToggle.classList.toggle('active')
+                showcase.classList.toggle('active')
+            })
+        
